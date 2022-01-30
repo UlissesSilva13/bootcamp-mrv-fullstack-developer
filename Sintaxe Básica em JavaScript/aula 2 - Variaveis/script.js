2868e5d2c364835be6646a8c86409f561fcee14d
Ulisses <ulisses.ssilva13@gmail.com>
2022-01-30 17:05:05 -0300

    Inclusão do curso Sintaxe Básica em JavaScript

---
+++ Sintaxe Básica em JavaScript/aula 2 - Variaveis/script.js
@@ -0,0 +1,102 @@
+// tipos primitivos
+
+// boolean
+/* var vOuF = false;
+console.log(typeof(vOuF));
+
+// number
+var numeroQualqer = 1;
+console.log(typeof(numeroQualqer));
+
+// string
+var nome = 'Ulisses';
+console.log(typeof(nome));
+
+// function
+var funcao = function() {}
+console.log(typeof(funcao)); */
+
+// como declarar
+/* var variavel = 'Ulisses';
+variavel = 'Joyce';
+console.log(variavel);
+
+let variavel2 = 'Ulisses';
+variavel2 = 'Joyce';
+console.log(variavel2); */
+
+/* const constante = 'Ana';
+constante = 'Martine';
+console.log(constante) */
+
+/* var escopoGlobal = 'glabal';
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopoLocalInterno = 'local';
+console.log(escopoLocalInterno);
+}
+
+escopoLocal(); */
+
+/* // atribuicao
+var atribuicao = 'Joyce';
+
+// comparacao
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+// comparcao identica
+var comparcaoIdentica = '0' === 0;
+console.log(comparcaoIdentica); */
+
+/* // adicao
+var adicao = 1 + 1;
+console.log(adicao);
+
+// subtracao
+var subtracao = 2 - 1;
+console.log(subtracao);
+
+// multiplicacao
+var multiplicacao = 2 * 3;
+console.log(multiplicacao);
+
+// divisao real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+// divisao inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+// potenciacao
+var potenciacao = 2 ** 10;
+console.log(potenciacao); */
+
+/* // maior que
+var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+// menor que
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+// maior ou igual a
+var maiorOuIgual = 5 > 2;
+console.log(maiorOuIgual);
+
+// menor ou igual 
+var menorOuIgual = 5 < 2;
+console.log(menorOuIgual); */
+
+
+/* // && todos os valores precisam ser verdadeiros
+var e = true && true;
+console.log(e);
+
+var ou = true || false;
+console.log(ou);
+
+var nao = !true;
+console.log(nao); */
---
+++ Sintaxe Básica em JavaScript/aula 4 - Estrutura condicionais/script.js
@@ -0,0 +1,43 @@
+let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5',];
+
+let object = { propriedade1: 'valor1', propriedade2: 'valor2', propriedade3: 'valor3', }
+
+// for executa uma instrução até que ela seja falsa
+/*for (let indice = 0; indice < array.length; indice++) {
+    console.log(indice);
+} */
+
+// for/in - executa repetição a partir de uma propriedade
+// com array
+/* for (let i in array) {
+    console.log(i);
+}
+
+//com abject
+for (i in object) {
+    console.log(i);
+} */
+
+// for/of - executa repetição a partir de valor
+// com array
+/*for (i of array) {
+    console.log(i);
+}
+
+// com object
+/* for (i of object.propriedade1) {
+    console.log(i);
+} */
+
+/* var a = 0;
+
+while (a < 10) {
+    a++;
+    console.log(a);
+} */
+
+/* do {
+    a++;
+    console.log(a);
+    while (a < 10);
+} */
