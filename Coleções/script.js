6f4992f8c43d407b1d88539d2b5483dce975f67d
Ulisses <ulisses.ssilva13@gmail.com>
2022-02-19 19:04:23 -0300

    Aula Coleções

---
+++ Coleções/script.js
@@ -0,0 +1,33 @@
+// Map
+/* const getAdmins = (map) => {
+	let admins = [];
+
+	for ([key, value] of map) {
+		if (value === 'ADMIN') {
+			admins.push(key);
+		}
+	}
+
+	return admins;
+};
+
+const userRoles = new Map();
+
+userRoles.set('Stephany', 'SUDO');
+userRoles.set('Luiz', 'ADMIN');
+userRoles.set('Elvira', 'ADMIN');
+userRoles.set('Carolina', 'USER');
+userRoles.set('Guilherme', 'USER');
+
+console.log(getAdmins(userRoles)); */
+
+
+// Sets
+/* function uniqueElements(array) {
+	let unique = new Set(array);
+	return [...unique];
+}
+
+const arr = [30, 30, 40, 5, 223, 2049, 3034, 5];
+
+console.log(uniqueElements(arr); */
